5c2a8e05fd5c46e3c5ad82bc6f59a0697a088845
AlvaroIsaac <63081656+AlvaroIsaac@users.noreply.github.com>
2020-05-15 20:27:05 -0300

    tercer commit

--- app/src/main/java/com/example/magicball/MainActivity.java
+++ app/src/main/java/com/example/magicball/MainActivity.java
@@ -15,16 +15,17 @@ public class MainActivity extends AppCompatActivity {
    private String[]respuestaArray={"Es cierto", "Definitivamente es así", "Sin duda", "Sí definitivamente", "Puede confiar en él", "Como yo lo veo, sí",
            "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde",
            "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso",
-           "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"}
+           "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ballI= findViewById(R.id.boton); // se refiere a la variable que en nuestra pantalla tiene la variable boton
         respuesta= findViewById(R.id.respuesta); //se refiere a la variable
 
         ballI.setOnClickListener(this); //constantemente escuchando  cada cierto tiempo pregunta si alguien a echo click
-        Toast.makeText(MainActivity.this, "buscando tu destino!!" Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "buscando tu destino!!", Toast.LENGTH_SHORT).show();
      }
      @Override
      public void onClick(View v){
